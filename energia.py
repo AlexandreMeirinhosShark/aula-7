1b0e1538877c6cf521deaced66832809d7aa4551
AlexandreMeirinhosShark <shark05980@sharkcoders.pt>
2024-11-15 18:26:22 +0000

    Update energia.py

--- energia.py
+++ energia.py
@@ -2,8 +2,8 @@ tipo = input("Qual é a sua instalação? (r -> Residencial, i ->Industrial, c -
 emc = float(input("Qual é a quantidade em kWh consumida? "))
 
 if tipo == "r":
-    print(f"Tem que pagar {round((emc * 0.0008),2)} euros")
+    print(f"Tem que pagar {round((emc * 0.525),2)} euros")
 if tipo == "i":
-    print(f"Tem que pagar {round((emc * 0.00055),2)} euros")
-if tipo == "c"
-    print(f"Tem que pagar {round((emc * 0.00011),2)} euros")
+    print(f"Tem que pagar {round((emc * 0.57),2)} euros")
+if tipo == "c":
+    print(f"Tem que pagar {round((emc * 0.58),2)} euros")
